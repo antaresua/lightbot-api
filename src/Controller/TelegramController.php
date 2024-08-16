6d6ada95de0f9a9809d2b026d57efebe42989658
= <antares@evoplay.tech>
2024-08-16 21:58:21 +0300

    fix maintenance methods

--- src/Controller/TelegramController.php
+++ src/Controller/TelegramController.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Service\TelegramService;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -11,32 +13,46 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Process\Process;
 use Symfony\Component\Routing\Attribute\Route;
 
+
 class TelegramController extends AbstractController
 {
+    public function __construct(
+        private readonly LoggerInterface $logger,
+        private readonly TelegramService $telegramService
+    ) {
+
+    }
+
     #[Route('/api/telegram/maintenance/on', name: 'api_telegram_set_maintenance', methods: ['POST'])]
-    public function setMaintenance(Request $request): JsonResponse
+    public function setMaintenance(): JsonResponse
     {
-        $process = new Process(['php', 'bin/console', 'app:notify:maintenance', 'on']);
-        $process->run();
+        $message = '⚠️ Канал зупинено на технічне обслуговування\.';
 
-        if (!$process->isSuccessful()) {
-            return new JsonResponse(['error' => $process->getErrorOutput()], Response::HTTP_INTERNAL_SERVER_ERROR);
+        try {
+            $this->telegramService->sendMessage($message);
+
+            return new JsonResponse(['message' => 'Channel set to maintenance mode.'], Response::HTTP_OK);
+        } catch (\Exception $exception) {
+            $this->logger->error('Failed to send Telegram message: ' . $exception->getMessage());
+
+            return new JsonResponse(['error' => $exception->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
-
-        return new JsonResponse(['message' => 'Channel set to maintenance mode.']);
     }
 
     #[Route('/api/telegram/maintenance/off', name: 'api_telegram_restore_service', methods: ['POST'])]
-    public function restoreService(Request $request): JsonResponse
+    public function restoreService(): JsonResponse
     {
-        $process = new Process(['php', 'bin/console', 'app:notify:maintenance', 'off']);
-        $process->run();
+        $message = '✅ Роботу каналу відновлено\.';
 
-        if (!$process->isSuccessful()) {
-            return new JsonResponse(['error' => $process->getErrorOutput()], Response::HTTP_INTERNAL_SERVER_ERROR);
+        try {
+            $this->telegramService->sendMessage($message);
+
+            return new JsonResponse(['message' => 'Channel restored.'], Response::HTTP_OK);
+        } catch (\Exception $exception) {
+            $this->logger->error('Failed to send Telegram message: ' . $exception->getMessage());
+
+            return new JsonResponse(['error' => $exception->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
-
-        return new JsonResponse(['message' => 'Channel restored to normal operation.']);
     }
 
     #[Route('/api/telegram/run-check-host', name: 'api_telegram_run_check_host', methods: ['POST'])]

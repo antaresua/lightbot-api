14dcd7ab47eeb17085a89a2150cc3fdb75f91524
Antares <antares@evoplay.tech>
2024-09-04 12:56:54 +0300

    fix DateRangeDTO

--- src/DTO/DateRangeDTO.php
+++ src/DTO/DateRangeDTO.php
@@ -6,7 +6,17 @@ namespace App\DTO;
 
 class DateRangeDTO
 {
-    public function __construct(public \DateTimeInterface $start, public \DateTimeInterface $end)
+    public function __construct(public \DateTimeInterface $startDate, public \DateTimeInterface $endDate)
     {
+    }
+
+    public function getStartDate(): \DateTimeInterface
+    {
+        return $this->startDate;
+    }
+
+    public function getEndDate(): \DateTimeInterface
+    {
+        return $this->endDate;
     }
 }
